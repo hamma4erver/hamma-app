08400dbbd26773bb6ec3128ff58799c35c617fc9
hamma4erver <ammamouhammed@gmail.com>
2026-07-11 23:18:04 +0100

    Delete public directory

--- public/js/auth.js
+++
@@ -1,9 +0,0 @@
-function getUsername() {
-    let user = localStorage.getItem('chat_username');
-    if (!user) {
-        user = prompt("أدخل اسمك للدخول إلى شات حمة:");
-        if (!user) user = "مجهول";
-        localStorage.setItem('chat_username', user);
-    }
-    return user;
-}
